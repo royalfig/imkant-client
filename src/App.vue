2ee581009ca03b5f101ab34e164062b2df2065fa
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2020-04-11 13:54:32 -0400

    updated styles

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,64 @@
 <template>
   <div id="app">
     <b-header />
+    <get-and-post-content />
     <post-control />
   </div>
 </template>
 
 <script>
-import PostControl from "./components/PostControl.vue";
 import Header from "./components/Header.vue";
+import GetAndPostContent from "./components/GetAndPostContent";
+import PostControl from "./components/PostControl.vue";
 
 export default {
   name: "App",
   components: {
-    PostControl,
-    "b-header": Header
+    "b-header": Header,
+    GetAndPostContent,
+    PostControl
   }
 };
 </script>
 
 <style lang="scss">
 @import "~bulma/sass/utilities/_all";
+
+$primary: #80deea;
+
+$primary-invert: findColorInvert($primary);
+$dark: #0d0e0f;
+$primary-invert: findColorInvert($dark);
+$danger: #fd3c01;
+$danger-invert: findColorInvert($danger);
+$colors: (
+  // "text": #f5f5f5,
+    // "textInverse": rgb(10, 10, 10),
+    // "text1": #e8e9e9,
+    // "text2": #c7c7c8,
+    // "text3": #a5a5a7,
+    // "text4": #616264,
+    // "surface": #09090a,
+    // "surface1": #0d0e0f,
+    // "surface2": #111214,
+    // "surface3": #1a1b1f,
+    // "surface4": #1d1e22,
+    "dark": ($dark, $dark-invert),
+  "primary": ($primary, $primary-invert),
+  "info": ($primary, $primary-invert),
+  "danger": ($danger, $danger-invert)
+);
+
+$link: $primary;
+
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+html {
+}
 #app {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-@import "~bulma";
-@import "~buefy/src/scss/buefy";
 </style>
---
+++ src/components/GetAndPostContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-6">
+        <h2 class="title is-3">Run the Kant Bot</h2>
+        <div class="buttons">
+          <b-button class icon-left="file-outline">Kant Studien</b-button>
+          <b-button class icon-left="file-outline">Kantian Review</b-button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
